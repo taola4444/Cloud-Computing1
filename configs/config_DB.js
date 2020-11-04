c6e45af3ae9aa687d0a0a71033252e5dcdabcbb5
taola4444 <58714744+taola4444@users.noreply.github.com>
2020-11-04 14:16:19 +0700

    Add files via upload

---
+++ configs/config_DB.js
@@ -0,0 +1,16 @@
+module.exports = {
+    localdb: {
+        urldb : 'mongodb+srv://phuc:khieVuk2ZM11p2s4@cluster0.skjiy.mongodb.net/shopbebe?retryWrites=true&w=majority',
+        dbname : "shopbebe",
+        dbusername : "",
+        dbpassword : "",
+    },
+
+    clouddb: {
+        urldb : 'mongodb+srv://phuc:khieVuk2ZM11p2s4@cluster0.skjiy.mongodb.net/shopbebe?retryWrites=true&w=majority',
+        dbname : "shopbebe",
+        dbusername : "",
+        dbpassword : "",
+    }
+
+};
